fa5d4f00b10dd22299854d9a7e4f2c02e3e1fcf1
Regina Zhyldyzbekova <regina.zhyldyzbekova@gmail.com>
2022-07-13 23:48:34 -0400

    first commit

---
+++ js/banki.js
@@ -0,0 +1,52 @@
+export const banki ={
+    questions: [
+        {
+            question: "Why this university?",
+            type: "education",
+        },
+        {
+            question: "Why do you plan to do MS? Why MS in CS?",
+            type: 'education',
+        },
+        {
+            question: 'Why do you wish to study in the US?',
+            type: 'education',
+        },
+        {
+            question: "Why Don't you study in your country?",
+            type: 'education'
+        }, 
+        {
+            question: 'Why [your major] as specialization?',
+            type: 'specialization ',
+        },
+        {
+            question: 'Which was your undergraduate college?',
+            type: 'specialization',
+        },
+        {
+            question: "How many admits/ rejects?",
+            type: "Uni Application",
+        },
+        {
+            question: "What was your first choice?",
+            type: 'Uni Application',
+        },
+        {
+            question: 'What is your GRE/TOEFL score?',
+            type: 'Uni Application',
+        },
+        {
+            question: "What are your grades like?",
+            type: 'Uni Application'
+        }, 
+        {
+            question: 'Did you have any backlogs?',
+            type: 'Uni Application',
+        },
+        {
+            question: "What was your bachelor's degree in?",
+            type: 'Uni Application',
+        },
+    ],
+};
---
+++ js/main.js
@@ -0,0 +1,19 @@
+document.querySelector("#clickMe").addEventListener("click", newQuestion);
+
+async function newQuestion() {
+	await makeReq()
+	document.querySelector("section").classList.add("animate__animated", "animate__bounceIn");
+	document.querySelector("section").classList.remove("hidden");
+	document.querySelector("section").addEventListener("animationend", () => {
+		document.querySelector("section").classList.remove("animate__animated");
+		document.querySelector("section").classList.remove("animate__bounceIn");
+	});
+}
+
+async function makeReq() {
+	const res = await fetch(`/api`);
+	const data = await res.json();
+	document.querySelector("#question").innerHTML = data.question;
+	let qType = data.type;
+	document.querySelector("#questionType").textContent = `${qType.charAt(0).toUpperCase() + qType.slice(1)}`;
+};
